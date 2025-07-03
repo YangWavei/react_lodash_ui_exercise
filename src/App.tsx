65b601a6d46c01ee2545d522d05fed53aa38d725
肖振洋 <3089326386@qq.com>
2025-07-03 16:31:34 +0800

    完成了Bar组件的开发

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Avatar from './components/Avatar';
+import Bar from './components/Bar';
 
 /** 主组件 */
 function App() {
   return (
     <div>
-      <div className='w-[100px] h-[100px]'>嗨嗨</div>
+      <Bar />
       <Avatar name='张三' age={20} sex={1} />
     </div>
   );
--- src/components/Bar.tsx
+++ src/components/Bar.tsx
@@ -1,26 +1,44 @@
-import { useState } from 'react';
-
+import { useState } from "react";
+import { Button } from 'antd'
 /** 将组件作为函数 */
 function Bar() {
-  const [arr, setArr] = useState<number[]>([1, 2, 3, 4, 5, 6]);
-  function handleDelOneLi(item: number) {
-    // immutable 不可变数据
-    setArr(arr.filter(ele => ele !== item));
+  const [count, setCount] = useState<number>(0)
+
+  function addCount() {
+    // 3种方式
+
+    //1、 可以设置新值
+    // setCount(handleCountAdd)
+    //2、采用函数式更新
+    //3、
+    setCount(prev => prev + 2)
+    // setCount(handleCountAdd) // 先执行函数，用其返回值作为新state
   }
+
+  /*   const handleCountAdd = (preState: number) => {
+      return preState + 1
+    }
+   */
+  
+  function decreaseCount() {
+    setCount(prev => prev - 2)
+  }
+
   return (
     <>
-      <ul>
-        {arr.map(ele => (
-          <li className='w-[300px] h-[40px] my-2  flex items-center justify-between px-10 bg-indigo-200' key={ele}>
-            {ele}
-            <span className='cursor-pointer text-[red]' onClick={() => handleDelOneLi(ele)}>
-              X
-            </span>
-          </li>
-        ))}
-      </ul>
+      <div className='w-[600px] h-[200px] bg-fuchsia-400'>
+        <span>bar组件 <strong>我是一个简单的计数器</strong></span>
+        <div className="h-[100px]">
+          <h2>{count}</h2>
+          <div className="w-full flex gap-2">
+            <Button type="primary" onClick={addCount}>增加</Button>
+            <Button onClick={decreaseCount}>减少</Button>
+          </div>
+        </div>
+
+      </div>
     </>
-  );
+  )
 }
 
 export default Bar;
