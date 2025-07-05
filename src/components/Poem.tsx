76aa1d0f56b697bec750411a85f53e40be28b58e
肖振洋 <3089326386@qq.com>
2025-07-05 15:22:47 +0800

    poem写法二

--- src/components/Poem.tsx
+++ src/components/Poem.tsx
@@ -10,31 +10,31 @@ const poem = {
 function Poem() {
   // todo 在每个`<p></p>`标签中插入 分割线
   // 注意，<hr /> 元素只应该在 <p> 元素 之间 出现，而不是在开头或结尾。
-  const output: ReactNode[] = []
+  // const output: ReactNode[] = []
 
   // 填装输出的数组
 
-  poem.lines.forEach((line, i) => {
-    output.push(<hr key={i + 'line'} />)
-    output.push(<p key={i + 'separate'}>{line}</p>)
-  })
+  // poem.lines.forEach((line, i) => {
+  //   output.push(<hr key={i + 'line'} />)
+  //   output.push(<p key={i + 'separate'}>{line}</p>)
+  // })
 
-  // 移除第一个 <hr/>
-  poem.lines.shift()
+  // // 移除第一个 <hr/>
+  // poem.lines.shift()
 
   return (
     <article>
-      {output}
+      {/* {output} */}
 
       {/* Fragment 相当于一个透明的袋子，用于包裹JSX元素 <></> 是它的简写形式 */}
-      {/* {poem.lines.map((ele, i) => {
+      {poem.lines.map((ele, i) => {
         return (
           <Fragment key={i + 'line'}>
             {i > 0 && <hr />}
             <p>{ele}</p>
           </Fragment>
         )
-      })} */}
+      })}
 
       <Fragment >
 
