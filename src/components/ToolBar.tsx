e48859ad9f2f83844a982e68c391977c5d6df15d
肖振洋 <3089326386@qq.com>
2025-07-11 16:36:12 +0800

    将事件作为prop传递

--- src/components/ToolBar.tsx
+++ src/components/ToolBar.tsx
@@ -2,7 +2,7 @@ import { Button } from "antd";
 import type { ReactNode } from "react";
 
 interface MyButtonParamsType {
-  onClick: () => void
+  onSmash: () => void
   children?: ReactNode
 }
 
@@ -10,42 +10,37 @@ interface MyButtonParamsType {
  * 接收一个名为onClick的prop，它将这个属性prop 以 onClick={onClick}
  * 方式传递给浏览器内置的 <button>。当点击按钮时，React 会调用传入的函数。
  */
-function MyButton({ onClick, children }: MyButtonParamsType) {
+function MyButton({ onSmash, children }: MyButtonParamsType) {
   return (
     <>
-      <Button onClick={onClick} >
+      <Button onClick={onSmash} >
         {children}
       </Button>
     </>
   )
 }
 
-
+/** 播放按钮 */
 function PlayButton({ movieName }: { movieName: string }) {
-  function handlePlayClick() {
-    alert(`正在播放${movieName}`)
+  const handlePlay = () => {
+    alert(`正在播放 "${movieName}"`)
   }
   return (
-    // 这里的onClick其实是父组件的属性，该属性是一个函数，这里我们使用handlePlayClick
-    // 作为`onClick` prop 传入 Button 组件内部
-    // 将组件从父组件接收的prop作为事件处理函数传递
-    <MyButton onClick={handlePlayClick}>
+    <MyButton onSmash={handlePlay}>
       播放 "{movieName}"
     </MyButton>
   )
 }
 
-
 function UploadButton() {
   return (
-    // 这里将 `() => alert(`正在上传`)` 作为 `onClick` prop 传入MyButton组件内部
-    <MyButton onClick={() => alert(`正在上传`)}>
-      上传图片
+    <MyButton onSmash={() => alert(`正在上传`)}>
+      开始上传
     </MyButton>
   )
 }
 
-
+/** 工具栏 */
 function ToolBar() {
   return (
     <div className="flex gap-2">
