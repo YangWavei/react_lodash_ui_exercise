2c4b5b8e1d293434d57bfa8c402345f2fc520d66
肖振洋 <3089326386@qq.com>
2025-07-11 16:29:22 +0800

    将事件处理函数作为props传递

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="w-screen h-screen bg-sky-100 flex justify-center  items-center">
       <img src='/images/react.svg' alt="react_logo" title="react_logo" />
+      <img src='/vite.svg' alt="react_logo" title="react_logo" />
       <ToolBar />
     </div>
   );
--- src/components/ToolBar.tsx
+++ src/components/ToolBar.tsx
@@ -1,15 +1,58 @@
-import AlertButton from "./AlertButton"
+import { Button } from "antd";
+import type { ReactNode } from "react";
+
+interface MyButtonParamsType {
+  onClick: () => void
+  children?: ReactNode
+}
+
+/** 自定义按钮
+ * 接收一个名为onClick的prop，它将这个属性prop 以 onClick={onClick}
+ * 方式传递给浏览器内置的 <button>。当点击按钮时，React 会调用传入的函数。
+ */
+function MyButton({ onClick, children }: MyButtonParamsType) {
+  return (
+    <>
+      <Button onClick={onClick} >
+        {children}
+      </Button>
+    </>
+  )
+}
+
+
+function PlayButton({ movieName }: { movieName: string }) {
+  function handlePlayClick() {
+    alert(`正在播放${movieName}`)
+  }
+  return (
+    // 这里的onClick其实是父组件的属性，该属性是一个函数，这里我们使用handlePlayClick
+    // 作为`onClick` prop 传入 Button 组件内部
+    // 将组件从父组件接收的prop作为事件处理函数传递
+    <MyButton onClick={handlePlayClick}>
+      播放 "{movieName}"
+    </MyButton>
+  )
+}
+
+
+function UploadButton() {
+  return (
+    // 这里将 `() => alert(`正在上传`)` 作为 `onClick` prop 传入MyButton组件内部
+    <MyButton onClick={() => alert(`正在上传`)}>
+      上传图片
+    </MyButton>
+  )
+}
+
 
 function ToolBar() {
   return (
-    <div className="flex  gap-2">
-      <AlertButton message="正在播放！">
-        播放电影
-      </AlertButton>
-      <AlertButton message="正在上传">
-        上传图片
-      </AlertButton>
+    <div className="flex gap-2">
+      <PlayButton movieName="魔女宅急便" />
+      <UploadButton />
     </div>
   )
 }
+
 export default ToolBar
