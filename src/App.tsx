0beca515ab31ed95ea9d01ab68967f6b7c23e0a6
肖振洋 <3089326386@qq.com>
2025-07-11 14:48:20 +0800

    useEffect函数的理解

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import './App.css'
-import Clock from './components/Clock';
+import { useEffect } from "react";
+import TeaGathering from "./components/TeaGathering";
+
 /** 主组件 */
 function App() {
+  // useEffect用于处理与组件渲染无关的内容
+  // 这会告诉React在渲染结束后执行它，类似与Vue中的 onMounted
+  // 组件挂载完成后执行
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      console.log(`1s后执行`);
+    }, 1000)
+    // 这里的回调函数相当于Vue3中的onBeforeUnmount()
+    // 在组件卸载之前调用
+    return () => clearTimeout(timer)
+  }, [])
+
   return (
     <div className="w-screen h-screen bg-sky-100">
-      {/* <div className="banner">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Dolorem esse soluta perspiciatis ducimus. Aliquam, provident nisi aspernatur
-        rem velit aliquid fuga iste nulla et atque quod ut molestiae, accusamus ratione?
-      </div>
-      <AnimateDemo /> */}
-      <Clock />
+      <TeaGathering />
     </div>
   );
 }
