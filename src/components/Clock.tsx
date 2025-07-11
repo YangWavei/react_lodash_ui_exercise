0531d240e11bf2c86890eb27cd70f74d30559680
肖振洋 <3089326386@qq.com>
2025-07-11 14:53:05 +0800

    修复损坏的时钟01

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,36 +1,18 @@
-import { useEffect, useRef, useState } from "react"
-
-/** 修复坏掉的时钟 */
-function Clock() {
-  // 日期对象
-  const [date, setDate] = useState(new Date())
-
-  // 得到DOM元素
-  const timeRef = useRef<HTMLHeadingElement>(null)
-
-  // 与组件render无关的内容全部放到 副作用函数中
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setDate(new Date())
-    }, 1000)
-    // 组件卸载时的清理函数
-    return () => clearInterval(timer)
-  }, [])
-
-  // 当依赖项date发生变化时，执行副作用函数中的逻辑
-  useEffect(() => {
-    if (timeRef.current) {
-      const hours = date.getHours()
-      timeRef.current.className = hours >= 0 && hours <= 6 ? 'night' : 'light'
-    }
-  }, [date])
-  
-
-  return (
-    <h1 ref={timeRef}>
-      {date.toLocaleTimeString()} - {date.toLocaleDateString()}
-    </h1>
-  )
+interface ClockParamsType {
+  time: Date
 }
-
-export default Clock
+export default function Clock({ time }: ClockParamsType) {
+  const hours = time.getHours()
+  // 计算className将其包含在渲染的输出中，以此实现对组件的修复
+  let className: string
+  if (hours >= 0 && hours <= 6) {
+    className = 'night'
+  } else {
+    className = 'day'
+  }
+  return (
+    <h1 id="time" className={className}>
+      {time.toLocaleTimeString()}
+    </h1>
+  );
+}
