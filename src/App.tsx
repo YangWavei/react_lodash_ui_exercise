fe733f17eaeb7363fcacb88551acd4b6839c79be
肖振洋 <3089326386@qq.com>
2025-07-12 13:10:49 +0800

    使用useImmer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import MovingDot from "./components/MovingDot";
+import MyForm from "./components/MyForm";
 
 /** 主组件 */
 function App() {
   return (
-    <div className="w-screen h-screen  bg-indigo-200 ">
-      <MovingDot />
+    <div className="w-screen h-screen ">
+      <MyForm />
     </div>
   );
 }
--- src/components/MyForm.tsx
+++ src/components/MyForm.tsx
@@ -1,64 +1,86 @@
-import { useState } from "react";
+import { useImmer } from "use-immer";
 
 function MyForm() {
-  const [person, setPerson] = useState({
-    firstName: 'Barbara',
-    lastName: 'Hepworth',
-    email: 'bhepworth@sculpture.com'
+  // 更新一个嵌套对象
+  const [person, updatePerson] = useImmer({
+    name: 'Niki de Saint Phalle',
+    artwork: {
+      title: 'Blue Nana',
+      city: 'Hamburg',
+      image: 'https://i.imgur.com/Sd1AgUOm.jpg',
+    }
   });
 
-  function handleFirstNameChange(e: React.ChangeEvent<HTMLInputElement>) {
-    setPerson({
-      ...person,
-      firstName: e.target.value,
+  function handleNameChange(e: React.ChangeEvent<HTMLInputElement>) {
+    updatePerson(draft => {
+      draft.name = e.target.value
+    })
+  }
+  /** 更新title */
+  function handleTitleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    updatePerson(draft => {
+      draft.artwork.title = e.target.value
     })
   }
 
-  function handleLastNameChange(e: React.ChangeEvent<HTMLInputElement>) {
-    setPerson({
-      ...person,
-      lastName: e.target.value,
+  function handleCityChange(e: React.ChangeEvent<HTMLInputElement>) {
+    updatePerson(draft => {
+      draft.artwork.city = e.target.value
     })
   }
 
-  function handleEmailChange(e: React.ChangeEvent<HTMLInputElement>) {
-    setPerson({
-      ...person,
-      email: e.target.value,
+  function handleImageChange(e: React.ChangeEvent<HTMLInputElement>) {
+    // 更改嵌套对象中的内容，需要通过深拷贝完全断开与原对象的引用关系
+    // 设置为新的数据
+    updatePerson(draft => {
+      draft.artwork.image = e.target.value
     })
   }
 
   return (
-
     <>
       <label>
-        First name:
+        Name:
         <input
-          value={person.firstName}
-          onChange={handleFirstNameChange}
+          value={person.name}
+          onChange={handleNameChange}
         />
       </label>
       <label>
-        Last name:
+        Title:
         <input
-          value={person.lastName}
-          onChange={handleLastNameChange}
+          value={person.artwork.title}
+          onChange={handleTitleChange}
         />
       </label>
       <label>
-        Email:
+        City:
         <input
-          value={person.email}
-          onChange={handleEmailChange}
+          value={person.artwork.city}
+          onChange={handleCityChange}
+        />
+      </label>
+      <label>
+        Image:
+        <input
+          value={person.artwork.image}
+          onChange={handleImageChange}
         />
       </label>
       <p>
-        {person.firstName}{' '}
-        {person.lastName}{' '}
-        ({person.email})
+        <i>{person.artwork.title}</i>
+        {' by '}
+        {person.name}
+        <br />
+        (located in {person.artwork.city})
       </p>
+      <img
+        src={person.artwork.image}
+        alt={person.artwork.title}
+      />
     </>
   )
+
 }
 
 export default MyForm;
