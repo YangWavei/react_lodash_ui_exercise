92c54d8b063ba0d3a9dc4fad55aaf6f59956974f
肖振洋 <3089326386@qq.com>
2025-07-12 11:23:40 +0800

    使用展开语法复制对象

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
-import { Divider } from "antd";
-import RequestTracker from "./components/RequestTracker";
+import MovingDot from "./components/MovingDot";
 
 /** 主组件 */
 function App() {
   return (
     <div className="w-screen h-screen  bg-indigo-200 ">
-      <div className="flex justify-center  items-center gap-2">
-        <img src='/images/react.svg' alt="react_logo" title="react_logo" />
-        <img src='/vite.svg' alt="react_logo" title="react_logo" />
-      </div>
-      <Divider />
-      <RequestTracker />
+      <MovingDot />
     </div>
   );
 }
