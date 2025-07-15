e9aa35aac837906f77e0fb1fd8a0e4ea7945cce9
肖振洋 <3089326386@qq.com>
2025-07-15 10:01:39 +0800

    修复列表中错位的state

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
-import Gallery from "./components/Gallery";
+import { useState } from "react";
+import { contacts } from "./data";
+import Contact from "./components/Contact";
 
 /** 主组件 */
 function App() {
+  const [reverse, setReverse] = useState(false);
+
+  const displayedContacts = [...contacts];
+  if (reverse) {
+    displayedContacts.reverse();
+  }
   return (
     <>
-      <Gallery />
+      <label>
+        <input
+          type="checkbox"
+          checked={reverse}
+          onChange={e => {
+            setReverse(e.target.checked)
+          }}
+        />{' '}
+        以相反的顺序显示
+      </label>
+      <ul>
+        {/* 让state 与每个特定的联系人 相联系
+          使用联系人id 作为 `key` 就可以修复中状态被重置的问题
+          state 与树中的位置相关联，key 让你可以指定一个特定的位置，而不依赖于顺序
+        */}
+        {displayedContacts.map(contact =>
+          <li key={contact.id}>
+            <Contact contact={contact} />
+          </li>
+        )}
+      </ul>
+
     </>
   )
 }
--- src/data/index.tsx
+++ src/data/index.tsx
@@ -1,3 +1,5 @@
-export default [
-
-]
+export const contacts = [
+  { id: 0, name: 'Alice', email: 'alice@mail.com' },
+  { id: 1, name: 'Bob', email: 'bob@mail.com' },
+  { id: 2, name: 'Taylor', email: 'taylor@mail.com' }
+];
