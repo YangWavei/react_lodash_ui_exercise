bb7bd0dd3f5c5480454369198465ee0d5a3c63c7
肖振洋 <3089326386@qq.com>
2025-07-24 14:20:28 +0800

    algorithm init

---
+++ src/Algorithm/chapter_array_and_linkedlist/array.ts
@@ -0,0 +1,5 @@
+export function arrayCommonMethod() {
+  const arr = [1, 2, 3, 4, 5]
+  arr.fill(9, 1, 5)
+  console.log("🚀 ~ arrayCommonMethod ~ arr:", arr)
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import HandsomeTable from "./components/HandsomeTable";
-
+import { arrayCommonMethod } from "./Algorithm/chapter_array_and_linkedlist/array";
 function App() {
+  arrayCommonMethod()
   return (
-    <div>
-      <HandsomeTable />
-    </div>
+    <div className="w-screen h-[30px] bg-sky-300 flex justify-center items-center">Algorithm</div>
   )
 };
 export default App;
