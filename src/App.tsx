73e4570cad98f01e40f5096661e1326c9a1c2986
肖振洋 <3089326386@qq.com>
2025-07-28 17:41:58 +0800

    array

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,6 @@
-import {
-  arrayCommonMethod,
-  groupByLength,
-  findActiveUser,
-  users,
-  splitSentences,
-  processProducts,
-  products
-} from "./Algorithm/chapter_array_and_linkedlist/array";
 function App() {
-  arrayCommonMethod()
-  // 准确判断一个数据的类型
-  console.log(groupByLength(['ts', 'node', 'deno', 'Java', 'JavaScript']));
-  console.log(findActiveUser(users)?.name);
-  console.log(splitSentences(['Web开发 工程实践', 'TS 强类型']));
-  console.log(processProducts(products, 0.14));
   return (
-    <div className="w-screen h-[30px] bg-sky-300 flex justify-center items-center">Algorithm</div>
+    <div className="w-screen h-[30px] font-bold flex justify-center items-center">Algorithm</div>
   )
 };
 export default App;
