4c04220cef57af8fbd875605e5b58a3ba1e61db7
肖振洋 <3089326386@qq.com>
2025-07-29 17:03:06 +0800

    map-exercise

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import {
-  _differenceBy
-} from "./util";
+import { demo03 } from "./demo/map";
 function App() {
-  console.log(_differenceBy([3.1, 2.2, 1.3], [4.4, 2.5], Math.floor));
-  console.log(_differenceBy([{ 'x': 2 }, { 'x': 1 }], [{ 'x': 1 }], 'x'));
+  demo03()
   return (
     <div className="w-screen h-[30px] font-bold flex justify-center items-center">Lodash</div>
   )
