6b4de87559a13987359b2ce51820b1f8f8d0d02c
肖振洋 <3089326386@qq.com>
2025-07-29 10:52:16 +0800

    _compact

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { _chunk, _chunk2, _chunk3, _chunk4 } from "./util";
+import {
+  _chunk, _chunk2, _chunk3, _chunk4,
+  _compact, _compact2, _compact3
+} from "./util";
 function App() {
-  const arr = [3, 5, 9, 'd']
-  console.log(_chunk4(arr, 0));
-  console.log(_chunk4(arr, 1));
-  console.log(_chunk4(arr, 2));
-  console.log(_chunk4(arr, 3));
+  const arr = [0, 1, false, 2, '', 3, undefined, null, NaN]
+  console.log(_compact(arr));
+  console.log(_compact2(arr));
+  console.log(_compact3(arr));
   return (
     <div className="w-screen h-[30px] font-bold flex justify-center items-center">Lodash</div>
   )
--- src/util/index.ts
+++ src/util/index.ts
@@ -1,1 +1,2 @@
 export { _chunk, _chunk2, _chunk3, _chunk4 } from './lodash/array/chunk';
+export { _compact, _compact2, _compact3 } from './lodash/array/compact'
