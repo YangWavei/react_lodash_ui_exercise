da171269638bb96f2968401fa1e423a5c044d449
肖振洋 <3089326386@qq.com>
2025-07-30 15:27:23 +0800

    在react中使用css的几种方式

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import MyTanstackTable from "./components/MyTanstackTable";
-
+import ZY_Button from "./components/uuiMortyDesign/Button/ZY-Button";
 function App() {
   return (
-    <MyTanstackTable />
+    <div className="w-screen h-screen ">
+      <ZY_Button className="" onClick={() => console.log('clicked!')}>
+        ZY
+      </ZY_Button>
+    </div>
   )
 };
 export default App;
