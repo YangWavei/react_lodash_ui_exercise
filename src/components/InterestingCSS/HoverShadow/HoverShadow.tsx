60d535bbc1f48bbf792b5eca961d1049de5e877d
肖振洋 <3089326386@qq.com>
2025-07-31 19:53:04 +0800

    鼠标悬浮文字阴影跳动效果

--- src/components/InterestingCSS/HoverShadow/HoverShadow.tsx
+++ src/components/InterestingCSS/HoverShadow/HoverShadow.tsx
@@ -1,18 +1,19 @@
 /** 鼠标悬浮阴影跳动效果 */
 function HoverShadow() {
   return (
-    <>
-      <div className='flex-center cursor-pointer w-[100px] h-[100px] hover:text-red-800 border border-sky-400'>
-        Text
+    <div>
+      <h1 className="
+        w-screen h-[60px] 
+        flex-center bg-amber-100 font-bold text-[24px] cursor-pointer text-[#bfdf76] 
+        hover:animate-(--hoverShadowAnimate)
+        ">
+        hello
+      </h1>
+      <hr className="my-2" />
+      <div className="w-[100px] h-[100px]  bg-indigo-500 hoverShadowBox">
+        你好
       </div>
-      {/* 设置外层主题 */}
-      <div data-theme="midnight">
-        {/* 内层元素使用变体控制样式 */}
-        {/* variant (变体) 简单来说就是 在xxx的条件下应用xxx的样式 */}
-        <div className="w-[70px] h-[70px] theme-midnight:bg-indigo-600"></div>
-        <div className="w-[70px] h-[70px] bg-red-300"></div>
-      </div>
-    </>
+    </div>
   )
 };
 
