15ebade1a8917fd69328478fa613323890441e5d
肖振洋 <3089326386@qq.com>
2025-08-01 17:36:04 +0800

    优化_findIndex函数的实现

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,49 @@
 import {
-  _chunk, _chunk2, _chunk3, _chunk4,
-  _compact, _compact2, _compact3,
+  _chunk,
+  _chunk2,
+  _chunk3,
+  _chunk4,
+  _compact,
+  _compact2,
+  _compact3,
   _concat,
-  _difference, _difference2, _difference3,
+  _difference,
+  _difference2,
+  _difference3,
   _differenceBy,
   _drop,
   _dropRight,
   _dropRightWhile,
   _fill,
-  _findIndex
+  _findIndex,
 } from "./util";
 
 function App() {
   const users = [
-    { 'user': 'barney', 'active': false },
-    { 'user': 'fred', 'active': false },
-    { 'user': 'pebbles', 'active': true }
+    { user: "barney", active: false },
+    { user: "fred", active: false },
+    { user: "pebbles", active: true },
   ];
-
-  console.log(_findIndex(users, function (o) { return o.user === 'barney' }))
-  console.log(_findIndex(users, { 'user': 'fred', 'active': false }));
-  console.log(_findIndex(users, ['active', false]));
-  console.log(_findIndex(users, 'active'));
-
-
+  console.log(_findIndex(users, (o) => o.user === "barney"));
+  // =>0
+  console.log(_findIndex(users, { user: "fred", active: false }));
+  console.log(_findIndex(users, ["active", false]));
+  console.log(_findIndex(users, "active"));
 
   interface ObjType {
-    name: string,
-    age: number
-  };
+    name: string;
+    age: number;
+  }
   type A = keyof ObjType;
   // keyof 接受一个对象类型作为参数，返回该对象的所有键名组成的联合类型
 
   const obj = {
-    name: 'hello',
-    age: 30
-  }
+    name: "hello",
+    age: 30,
+  };
   // 使用 typeof 运算符返回 某个操作数的TS类型
   type B = typeof obj;
 
-  return (
-    <div className="w-screen h-[30px] font-bold flex justify-center items-center">Lodash</div>
-  )
-};
+  return <div className="w-screen h-[30px] font-bold flex justify-center items-center">Lodash</div>;
+}
 export default App;
--- src/util/lodash/array/findIndex.ts
+++ src/util/lodash/array/findIndex.ts
@@ -1,51 +1,56 @@
+export function _findIndex<T>(array: T[], predicate: I_predicate<T>, fromIndex = 0) {
+  if (!array || array.length === 0) return -1;
+  // 标准化 predicate 为函数形式
+  const predicateFn = createPredicateFn(predicate);
+  for (let i = fromIndex; i < array.length; i++) {
+    if (predicateFn(array[i], i, array)) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+/* -------------------------------------------------------------------------- */
+// 1、函数类型
+// 2、对象类型 (Partial<Type> 返回一个新类型，将参数类型 Type 的所有属性变为可选属性)
+// 3、数组类型
+// 4、字符串类型
+type PredicateFunction<T> = (value: T, index: number, arr: T[]) => boolean;
+
+type I_predicate<T> = PredicateFunction<T> | Partial<T> | [keyof T, any] | string;
+
+enum ParamType {
+  String = "String",
+  Function = "Function",
+  Array = "Array",
+  Object = "Object",
+}
+
 // 对象：匹配对象属性
 // 数组：匹配属性名和值（如 ['active', false]）
 // 字符串：匹配指定属性为真的元素
-// 函数：匹配经过函数调用后返回true的元素
-export function _findIndex<T>(
-  array: T[],
-  predicate: I_predicate<T> | Partial<T> | [keyof T, any] | string,
-  fromIndex = 0) {
-  // 边界值处理
-  if (!array || array.length === 0) return -1;
-  // 标准化 predicate 为函数形式
-  const predicateFn = createPredicate(predicate);
-  for (let i = fromIndex; i < array.length; i++) {
-    // 这个 `predicateFn` 会在每一次迭代调用
-    if (predicateFn(array[i], i, array)) return i;
-  }
-  return -1;
-};
+// 函数：匹配经过函数调用后返回true
+function createPredicateFn<T>(predicate: I_predicate<T>): PredicateFunction<T> {
+  const stringTypeCase = determineOnlyType(predicate) === ParamType.String;
+  const arrayTypeCase = determineOnlyType(predicate) === ParamType.Array;
+  const funcTypeCase = determineOnlyType(predicate) === ParamType.Function;
+  const objTypeCase = determineOnlyType(predicate) === ParamType.Object;
+  
+  if (funcTypeCase) return predicate as PredicateFunction<T>;
 
-/* -------------------------------------------------------------------------- */
-type I_predicate<T> = (value: T, index: number, arr: T[]) => boolean
-
-// 该函数的返回值类型一定是一个判断函数
-function createPredicate<T>(predicate: I_predicate<T> | Partial<T> | [keyof T, any] | string): I_predicate<T> {
-  // 1、predicate是函数的情况
-  const funcCase = determineDataType(predicate) === 'Function';
-  // 2、predicate是非数组对象的情况,匹配的是对象属性
-  const notArrayObjectCase = determineDataType(predicate) === 'Object' && determineDataType(predicate) !== 'Array';
-  // 3、predicate是数组的情况,匹配的是属性名和属性值(如['active',false])
-  const isArrayTypeCase = determineDataType(predicate) === 'Array';
-  // 4、predicate是字符串的情况，匹配指定属性为true的元素
-  const stringTypeCase = determineDataType(predicate) === 'String';
-
-  if (funcCase) return predicate as I_predicate<T>;
-
-  if (notArrayObjectCase) {
+  // 匹配对象的属性
+  if (objTypeCase) {
     return (item: T) => {
       for (const key in predicate as Partial<T>) {
         if ((predicate as Partial<T>)[key] !== item[key]) {
-          return false
+          return false;
         }
       }
-      return true
-    }
+      return true;
+    };
   }
-
-  if (isArrayTypeCase) {
-    // 数组匹配
+  // 数组：匹配属性名和值
+  if (arrayTypeCase) {
     const [key, value] = predicate as [keyof T, any];
     return (item: T) => item[key] === value;
   }
@@ -54,16 +59,13 @@ function createPredicate<T>(predicate: I_predicate<T> | Partial<T> | [keyof T, a
     // 检查属性是否存在且属性值是否为真
     return (item: T) => {
       const value = item[predicate as keyof T];
-      // !!符号表示将一个值转换为其对应的Boolean类型
       return !!value;
-    }
+    };
   }
 
   return () => false;
 }
 
-/** 准确判断数据类型 */
-function determineDataType(data: any) {
-  return Object.prototype.toString.call(data).slice(8, -1)
+function determineOnlyType(data: any) {
+  return Object.prototype.toString.call(data).slice(8, -1);
 }
-
