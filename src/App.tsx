37173c728e82aa396cfd022e12be0c7fbbc63e1e
肖振洋 <3089326386@qq.com>
2025-08-01 20:38:34 +0800

    优化_findIndex_02

--- src/App.tsx
+++ src/App.tsx
@@ -25,24 +25,9 @@ function App() {
     { user: "pebbles", active: true },
   ];
   console.log(_findIndex(users, (o) => o.user === "barney"));
-  // =>0
   console.log(_findIndex(users, { user: "fred", active: false }));
   console.log(_findIndex(users, ["active", false]));
   console.log(_findIndex(users, "active"));
-
-  interface ObjType {
-    name: string;
-    age: number;
-  }
-  type A = keyof ObjType;
-  // keyof 接受一个对象类型作为参数，返回该对象的所有键名组成的联合类型
-
-  const obj = {
-    name: "hello",
-    age: 30,
-  };
-  // 使用 typeof 运算符返回 某个操作数的TS类型
-  type B = typeof obj;
 
   return <div className="w-screen h-[30px] font-bold flex justify-center items-center">Lodash</div>;
 }
--- src/util/lodash/array/findIndex.ts
+++ src/util/lodash/array/findIndex.ts
@@ -1,46 +1,47 @@
+/**
+ * 返回第一个通过 predicate 判断为true的元素的索引值 (index)
+ *
+ * @template T
+ * @param {T} array 要搜索的数组
+ * @param {*} predicate 每次迭代时调用
+ * @param {number} [fromIndex=1] The index to search from.
+ */
 export function _findIndex<T>(array: T[], predicate: I_predicate<T>, fromIndex = 0) {
   if (!array || array.length === 0) return -1;
-  // 标准化 predicate 为函数形式
+  // 参数 predicate 有多种类型，因此需要将该参数统一成一个处理函数
+  // 下面先考虑最简单的一种类型，predicate为一个处理函数
   const predicateFn = createPredicateFn(predicate);
   for (let i = fromIndex; i < array.length; i++) {
     if (predicateFn(array[i], i, array)) {
+      // 通过 predicate 判断传入的数组元素是否为真，若为true,则返回当前的数组索引
       return i;
     }
   }
   return -1;
 }
 
-/* -------------------------------------------------------------------------- */
-// 1、函数类型
-// 2、对象类型 (Partial<Type> 返回一个新类型，将参数类型 Type 的所有属性变为可选属性)
-// 3、数组类型
-// 4、字符串类型
-type PredicateFunction<T> = (value: T, index: number, arr: T[]) => boolean;
+/**
+ * 函数类型
+ * @param value 数组中当前正在处理的元素
+ * @param index 正在处理的元素在数组中的索引
+ * @param arr 调用了 predicate() 的数组本身
+ */
+type FuncPredicateType<T> = (value: T, index: number, arr: T[]) => boolean;
 
-type I_predicate<T> = PredicateFunction<T> | Partial<T> | [keyof T, any] | string;
+// 实际上 predicate 具备多种类型
+type I_predicate<T> = FuncPredicateType<T> | string | [keyof T, any] | Partial<T>;
 
-enum ParamType {
-  String = "String",
-  Function = "Function",
-  Array = "Array",
-  Object = "Object",
-}
+function createPredicateFn<T>(predicate: I_predicate<T>): FuncPredicateType<T> {
+  const isObjectType = checkDataType(predicate) === "Object";
+  const isArrayType = checkDataType(predicate) === "Array";
+  const isStringType = checkDataType(predicate) === "String";
+  const isFuncType = checkDataType(predicate) === "Function";
+  if (isFuncType) return predicate as FuncPredicateType<T>;
 
-// 对象：匹配对象属性
-// 数组：匹配属性名和值（如 ['active', false]）
-// 字符串：匹配指定属性为真的元素
-// 函数：匹配经过函数调用后返回true
-function createPredicateFn<T>(predicate: I_predicate<T>): PredicateFunction<T> {
-  const stringTypeCase = determineOnlyType(predicate) === ParamType.String;
-  const arrayTypeCase = determineOnlyType(predicate) === ParamType.Array;
-  const funcTypeCase = determineOnlyType(predicate) === ParamType.Function;
-  const objTypeCase = determineOnlyType(predicate) === ParamType.Object;
-  
-  if (funcTypeCase) return predicate as PredicateFunction<T>;
-
-  // 匹配对象的属性
-  if (objTypeCase) {
+  // 匹配对象属性
+  if (isObjectType) {
     return (item: T) => {
+      // 传入对象的某某属性值与当前predicate参数中某某属性值不同则返回false,否则返回true
       for (const key in predicate as Partial<T>) {
         if ((predicate as Partial<T>)[key] !== item[key]) {
           return false;
@@ -49,23 +50,38 @@ function createPredicateFn<T>(predicate: I_predicate<T>): PredicateFunction<T> {
       return true;
     };
   }
-  // 数组：匹配属性名和值
-  if (arrayTypeCase) {
+
+  // 匹配属性名和属性值
+  if (isArrayType) {
     const [key, value] = predicate as [keyof T, any];
+    // 判断传入的某某属性名对应的属性值是否和 predicate中的相同，例如['active',false]。
+    // 是则返回 true
     return (item: T) => item[key] === value;
   }
 
-  if (stringTypeCase) {
-    // 检查属性是否存在且属性值是否为真
+  // 匹配指定属性为真的元素
+  if (isStringType) {
     return (item: T) => {
+      // keyof 关键字接受一个接口类型，返回接口类型所有键的联合类型
       const value = item[predicate as keyof T];
-      return !!value;
+      return Boolean(value);
     };
   }
 
   return () => false;
 }
 
-function determineOnlyType(data: any) {
+function checkDataType(data: any) {
   return Object.prototype.toString.call(data).slice(8, -1);
 }
+
+const obj = {
+  name: "占山",
+  age: 201,
+};
+
+const Test: keyof typeof obj = "name";
+
+console.log(Test);
+// keyof 接受一个对象(接口)类型作为参数，返回该对象的所有键名组成的联合类型
+// 往往用于精确表达对象的属性类型。
