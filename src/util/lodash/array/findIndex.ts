fc027e184abfc521c4ba2102eabeb160b77bd878
肖振洋 <3089326386@qq.com>
2025-08-02 11:41:26 +0800

    优化_findIndex_03

--- src/util/lodash/array/findIndex.ts
+++ src/util/lodash/array/findIndex.ts
@@ -8,80 +8,74 @@
  */
 export function _findIndex<T>(array: T[], predicate: I_predicate<T>, fromIndex = 0) {
   if (!array || array.length === 0) return -1;
-  // 参数 predicate 有多种类型，因此需要将该参数统一成一个处理函数
-  // 下面先考虑最简单的一种类型，predicate为一个处理函数
+  // 统一化为 predicate 函数
   const predicateFn = createPredicateFn(predicate);
   for (let i = fromIndex; i < array.length; i++) {
+    // 找到由 predicate 函数判断为真的元素索引值 ，否则返回 -1
     if (predicateFn(array[i], i, array)) {
-      // 通过 predicate 判断传入的数组元素是否为真，若为true,则返回当前的数组索引
       return i;
     }
   }
   return -1;
 }
 
-/**
- * 函数类型
- * @param value 数组中当前正在处理的元素
- * @param index 正在处理的元素在数组中的索引
- * @param arr 调用了 predicate() 的数组本身
- */
 type FuncPredicateType<T> = (value: T, index: number, arr: T[]) => boolean;
 
-// 实际上 predicate 具备多种类型
+// 实际上 predicate 有多种类型
+// let's discuss the following categories separately.
+// 1.Function => Return directly as a function.
+// 2.Object => 需要构造一个函数，匹配predicate 对象中的属性是否和array中的元素是否匹配
+//    (注意传入的predicate属性可能是 array中元素的一部分属性，是可选的)
+// 3.Array [属性名，属性值] 构造一个函数，匹配 array 数组中元素的属性名与属性值是否匹配
+// 4.字符串，匹配array中元素的 当前'字符串'对应的属性是否返回 true
+// (其实我们这里默认T是一个对象类型)
+
+// keyof 接受一个对象(接口)类型作为参数,返回该对象类型的所有键名组成的联合类型
+// Partial<Type>  返回一个新的类型,将参数类型 Type的所有属性变为可选属性
+
 type I_predicate<T> = FuncPredicateType<T> | string | [keyof T, any] | Partial<T>;
 
 function createPredicateFn<T>(predicate: I_predicate<T>): FuncPredicateType<T> {
-  const isObjectType = checkDataType(predicate) === "Object";
-  const isArrayType = checkDataType(predicate) === "Array";
-  const isStringType = checkDataType(predicate) === "String";
-  const isFuncType = checkDataType(predicate) === "Function";
-  if (isFuncType) return predicate as FuncPredicateType<T>;
+  // 根据传入的 predicate 数据类型分类讨论
+  const predicateType = determineDataType(predicate);
+  const isStringTypeCase = predicateType === choiceType.String;
+  const isObjectTypeCase = predicateType === choiceType.Object;
+  const isArrayTypeCase = predicateType === choiceType.Array;
+  const isFunctionTypeCase = predicateType === choiceType.Function;
 
-  // 匹配对象属性
-  if (isObjectType) {
+  if (isFunctionTypeCase) return predicate as FuncPredicateType<T>;
+
+  if (isObjectTypeCase) {
     return (item: T) => {
-      // 传入对象的某某属性值与当前predicate参数中某某属性值不同则返回false,否则返回true
+      // for...in 循环迭代一个对象的所有 可枚举字符串属性 (Symbol属性除外)
       for (const key in predicate as Partial<T>) {
-        if ((predicate as Partial<T>)[key] !== item[key]) {
+        if (item[key] !== (predicate as Partial<T>)[key]) {
           return false;
         }
       }
-      return true;
+      return true; // predicate 在array中是存在的
     };
   }
 
-  // 匹配属性名和属性值
-  if (isArrayType) {
+  if (isArrayTypeCase) {
     const [key, value] = predicate as [keyof T, any];
-    // 判断传入的某某属性名对应的属性值是否和 predicate中的相同，例如['active',false]。
-    // 是则返回 true
     return (item: T) => item[key] === value;
   }
 
-  // 匹配指定属性为真的元素
-  if (isStringType) {
-    return (item: T) => {
-      // keyof 关键字接受一个接口类型，返回接口类型所有键的联合类型
-      const value = item[predicate as keyof T];
-      return Boolean(value);
-    };
+  if (isStringTypeCase) {
+    return (item: T) => Boolean(item[predicate as keyof T]);
   }
 
   return () => false;
 }
 
-function checkDataType(data: any) {
+function determineDataType(data: any) {
   return Object.prototype.toString.call(data).slice(8, -1);
 }
 
-const obj = {
-  name: "占山",
-  age: 201,
-};
-
-const Test: keyof typeof obj = "name";
-
-console.log(Test);
-// keyof 接受一个对象(接口)类型作为参数，返回该对象的所有键名组成的联合类型
-// 往往用于精确表达对象的属性类型。
+enum choiceType {
+  String = "String",
+  Function = "Function",
+  Array = "Array",
+  Object = "Object",
+}
