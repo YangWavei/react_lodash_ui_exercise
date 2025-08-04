0f3106f42152178eafeea841728eb4079d496ab0
肖振洋 <3089326386@qq.com>
2025-08-04 20:30:56 +0800

    finish _zip_02

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import { _zip, _zip2 } from "./util";
 function App() {
   const res = _zip(["fred", "barney"], [30, 40], [true, false]);
   console.log("🚀 ~ App ~ res:", res);
-  _zip2(res);
+  const res2 = _zip2(["fred", "barney"], [30, 40], [true, false]);
+  console.log("🚀 ~ App ~ res2:", res2);
+
   return <div className="w-screen h-[30px] font-bold flex justify-center items-center">Lodash</div>;
 }
 export default App;
