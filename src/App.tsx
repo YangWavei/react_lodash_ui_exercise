57401243620654b7adf8e43dc7f0a88246b3c2e5
肖振洋 <3089326386@qq.com>
2025-08-09 10:15:19 +0800

    finish _boil

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,22 @@
-import { _alphabetical } from "./util/index";
-function App() {
-  const goods = [
-    {
-      name: 'Ra',
-      power: 100
-    },
-    {
-      name: 'Zeus',
-      power: 98
-    },
-    {
-      name: 'Loki',
-      power: 72
-    },
-    {
-      name: 'Vishnu',
-      power: 100
-    }
-  ];
-  const sortRes1 = _alphabetical(goods, good => good.name, 'desc');
-  const sortRes2 = _alphabetical(goods, good => good.name);
+import { _alphabetical, _boil } from "./util/index";
+const goods = [
+  {
+    name: 'Ra',
+    power: 100
+  },
+  {
+    name: 'Zeus',
+    power: 98
+  },
+  {
+    name: 'Loki',
+    power: 72
+  },
+];
 
-  console.log("🚀 ~ App ~ sortRes1:", sortRes1);
-  console.log("🚀 ~ App ~ sortRes2:", sortRes2);
+function App() {
+  const res = _boil(goods, (a, b) => (a.power > b.power) ? a : b);
+  console.log("🚀 ~ App ~ res:", res);
 
   return (
     <div className="w-screen h-screen  bg-yellow-50/60">
--- src/util/radash/array.ts
+++ src/util/radash/array.ts
@@ -23,3 +23,23 @@ export function _alphabetical<T>(
 
   return array.slice().sort(dir === 'desc' ? desc : asc);
 }
+
+/* -------------------------------------------------------------------------- */
+
+/**
+ * Reduce a list of items down to one item.
+ * Given an array of items return the final item that wins the comparison condition.
+ * Useful for more complicated min/max.
+ * @param array 
+ * @returns 
+ */
+export function _boil<T>(array: readonly T[], compareFunc: (a: T, b: T) => T): T | null {
+  // Null 判断运算符 `??` 只有在运算符左侧的值为 null 或者 undefined 时，才会返回右侧的值
+  if (!array || (array.length ?? 0) === 0) return null;
+  // reduce 方法 对数组中的每个元素执行 compareFunc 函数，从左到右将函数应用到每个元素，
+  // 最终将数组简化为单个值。比较函数决定在每一步中保留哪个元素，最终返回经过一系列比较后胜出的
+  // 元素。
+  // reduce() 方法对数组中的每个元素按顺序执行一个提供的 `reducer` 函数,每次运行reducer会将
+  // 先前计算的结果作为参数传入，最后将结果汇总为单个返回值
+  return array.reduce(compareFunc);
+}
