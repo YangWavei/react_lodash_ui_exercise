f4664ee853afbbc687a25ae72a46b68ce1c4d44d
肖振洋 <3089326386@qq.com>
2025-08-11 18:04:25 +0800

    重构文件组织方式

--- src/components/InterestingCSS/3_TextGradient/TextGradient.tsx
+++
@@ -1,9 +0,0 @@
-export default function TextGradient() {
-  return (
-    <>
-      <span className="gradientFlowingLightText font-bold text-[2rem] select-none cursor-pointer">
-        文字颜色渐变流光效果
-      </span>
-    </>
-  );
-}
--- src/components/InterestingCSS/4_TypewriterEffect/TypewriterEffect.tsx
+++
@@ -1,9 +0,0 @@
-export default function TypewriterEffect() {
-  const str = "渲染代码位于组件的顶层，你在这里处理它们进行转换。";
-  console.log(str.length);
-  return (
-    <h1 className="w-[25em] animate-typewriteEffectAnimate border-r-[1px] bg-amber-200/30 whitespace-nowrap overflow-hidden">
-      {str}
-    </h1>
-  );
-}
