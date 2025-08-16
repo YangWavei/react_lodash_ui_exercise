333d33eabb4b16045a4ca7b70d9e08e5a88dde79
肖振洋 <3089326386@qq.com>
2025-08-16 11:21:27 +0800

    体验 DynamicIcon

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -1,11 +1,22 @@
-import { Outlet } from "react-router";
+import { Camera } from "lucide-react";
+import { DynamicIcon } from "lucide-react/dynamic";
+import { useState } from "react";
+const iconlist = ['camera', 'armchair', 'rocking-chair', 'toggle-right', 'panel-top-dashed'] as const;
 
 export default function Index() {
+  const [iconName, setIconName] = useState<(typeof iconlist)[number]>('camera');
+
+  const handleToggleIcon = () => {
+    const randomIdx = Math.floor(Math.random() * iconlist.length);
+    setIconName(() => iconlist[randomIdx]);
+  };
+
   return (
     <div>
       <div className="text-[40px] font-bold">测试</div>
-      {/* 插槽 */}
-      <Outlet />
+      <Camera color="red" size={100} strokeWidth={1} fill="yellow" />
+      <h1 className="font-bold">DynamicIcon</h1>
+      <DynamicIcon className="cursor-pointer" name={iconName} onClick={handleToggleIcon} size={100} />
     </div>
   );
 };
