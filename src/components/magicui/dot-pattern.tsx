762d4e90eabcdf2963f08ea496146bb4dfe8c8dd
肖振洋 <3089326386@qq.com>
2025-08-16 17:32:40 +0800

    fix bug

--- src/components/magicui/dot-pattern.tsx
+++ src/components/magicui/dot-pattern.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { cn } from "@/lib/utils";
 import { motion } from "motion/react";
 import React, { useEffect, useId, useRef, useState } from "react";
+import { cn } from "../../lib/utils";
 
 /**
  *  DotPattern Component Props
