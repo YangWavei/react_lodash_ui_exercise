87ce904e05352b22d0c4d576c8d73746c67d4a04
肖振洋 <3089326386@qq.com>
2025-08-16 17:21:58 +0800

    fix bug

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -1,4 +1,4 @@
-import { DotPattern } from "@/components/magicui/dot-pattern";
+import { DotPattern } from "../../components/magicui/dot-pattern";
 
 export default function Index() {
 
@@ -6,7 +6,7 @@ export default function Index() {
     <div className="w-full h-full text-[30px]">
       {/* 背景 */}
       <DotPattern className="absolute -z-50 flex" />
-      <nav className="h-[8vh] w-screen bg-red-400/40 flex justify-between">
+      <nav className="h-[60px] w-screen bg-red-400/40 flex justify-between">
         <img className="w-[40px] h-[40px] flex-1/7" src="/vite.svg" alt="" />
         <ul className="w-[400px] bg-[#a4a4a4] flex-6/7 flex justify-around"></ul>
       </nav>
